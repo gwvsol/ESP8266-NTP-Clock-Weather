501b9b3757462df8798d5fdfeec9027d1d9ee7b9
gwvsol <gwvsol@gmail.com>
2019-04-14 23:19:49 +0300

    добавлены методы для работы с SPIFFS

--- include/BoardHTTP.h
+++ include/BoardHTTP.h
@@ -12,17 +12,20 @@
 ESP8266WebServer HTTP(WEBPORT);      // Web интерфейс для устройства
 File fsUploadFile;                   // Для работы с файловой системой
 
-// Ответ при обращении к главной странице
-void handleRoot() {
-    HTTP.send(200, "text/plain", "hello from esp8266!");
-}
-
-// Перезагрузка модуля по запросу вида http://IP/restart?device=ok
+// Перезагрузка модуля по запросу вида http://IP/rst?rst=ok
 void handle_Restart() {
     String restart = HTTP.arg("rst");
     if (restart == "ok") {
-        HTTP.send(200, "text/plain", "DEVICE RESTART OK");
+        HTTP.send(200, "text/plain", "OK");
         ESP.restart();
+    }
+}
+
+void save_Config() {
+    String save = HTTP.arg("save");
+    if (save == "ok") {
+        SaveConFig();
+        HTTP.send(200, "text/plain", "OK");
     }
 }
 
@@ -50,17 +53,141 @@ void handle_ntp_server() {
     HTTP.send(200, "text/plain", "OK");
 }
 
+// Методы для работы с SPIFFS
+String getContentType(String filename) {
+    if (HTTP.hasArg("download")) return "application/octet-stream";
+    else if (filename.endsWith(".htm")) return "text/html";
+    else if (filename.endsWith(".html")) return "text/html";
+    else if (filename.endsWith(".json")) return "application/json";
+    else if (filename.endsWith(".css")) return "text/css";
+    else if (filename.endsWith(".js")) return "application/javascript";
+    else if (filename.endsWith(".png")) return "image/png";
+    else if (filename.endsWith(".gif")) return "image/gif";
+    else if (filename.endsWith(".jpg")) return "image/jpeg";
+    else if (filename.endsWith(".ico")) return "image/x-icon";
+    else if (filename.endsWith(".xml")) return "text/xml";
+    else if (filename.endsWith(".pdf")) return "application/x-pdf";
+    else if (filename.endsWith(".zip")) return "application/x-zip";
+    else if (filename.endsWith(".gz")) return "application/x-gzip";
+    return "text/plain";
+}
+
+bool handleFileRead(String path) {
+    if (path.endsWith("/")) path += "index.htm";
+    String contentType = getContentType(path);
+    String pathWithGz = path + ".gz";
+    if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) {
+        if (SPIFFS.exists(pathWithGz))
+        path += ".gz";
+        File file = SPIFFS.open(path, "r");
+        size_t sent = HTTP.streamFile(file, contentType);
+        file.close();
+        return true;
+    }
+    return false;
+}
+
+void handleFileUpload() {
+    if (HTTP.uri() != "/edit") return;
+    HTTPUpload& upload = HTTP.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+        String filename = upload.filename;
+        if (!filename.startsWith("/")) filename = "/" + filename;
+        fsUploadFile = SPIFFS.open(filename, "w");
+        filename = String();
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+        //DBG_OUTPUT_PORT.print("handleFileUpload Data: "); DBG_OUTPUT_PORT.println(upload.currentSize);
+        if (fsUploadFile)
+        fsUploadFile.write(upload.buf, upload.currentSize);
+    } else if (upload.status == UPLOAD_FILE_END) {
+        if (fsUploadFile)
+        fsUploadFile.close();
+    }
+}
+
+void handleFileDelete() {
+    if (HTTP.args() == 0) return HTTP.send(500, "text/plain", "BAD ARGS");
+    String path = HTTP.arg(0);
+    if (path == "/")
+        return HTTP.send(500, "text/plain", "BAD PATH");
+    if (!SPIFFS.exists(path))
+        return HTTP.send(404, "text/plain", "FileNotFound");
+    SPIFFS.remove(path);
+    HTTP.send(200, "text/plain", "");
+    path = String();
+}
+
+void handleFileCreate() {
+    if (HTTP.args() == 0)
+        return HTTP.send(500, "text/plain", "BAD ARGS");
+    String path = HTTP.arg(0);
+    if (path == "/")
+        return HTTP.send(500, "text/plain", "BAD PATH");
+    if (SPIFFS.exists(path))
+        return HTTP.send(500, "text/plain", "FILE EXISTS");
+    File file = SPIFFS.open(path, "w");
+    if (file)
+        file.close();
+    else
+        return HTTP.send(500, "text/plain", "CREATE FAILED");
+    HTTP.send(200, "text/plain", "");
+    path = String();
+}
+
+void handleFileList() {
+    if (!HTTP.hasArg("dir")) {
+        HTTP.send(500, "text/plain", "BAD ARGS");
+        return;
+    }
+    String path = HTTP.arg("dir");
+    Dir dir = SPIFFS.openDir(path);
+    path = String();
+    String output = "[";
+    while (dir.next()) {
+        File entry = dir.openFile("r");
+        if (output != "[") output += ',';
+        bool isDir = false;
+        output += "{\"type\":\"";
+        output += (isDir) ? "dir" : "file";
+        output += "\",\"name\":\"";
+        output += String(entry.name()).substring(1);
+        output += "\"}";
+        entry.close();
+    }
+    output += "]";
+    HTTP.send(200, "text/json", output);
+}
+
 void HTTP_init(void) {
     // Главная страница http://IP/
-    HTTP.on("/", handleRoot);
     // Перезагрузка модуля по запросу вида http://IP/restart?rst=ok
-    HTTP.on("/restart", handle_Restart);
+    HTTP.on("/rst", handle_Restart);
     // Синхронизация времени устройства по запросу вида http://IP/time
     HTTP.on("/time", handle_Time);
     // Установка временной зоны по запросу вида http://IP/timez?timez=3
     HTTP.on("/timez", handle_time_zone);
     // Установка нового NTP сервера по запросу вида http://IP/setntp?setntpz=us.pool.ntp.org
     HTTP.on("/setntp", handle_ntp_server);
+    // HTTP страницы для работы с SPIFFS
+    HTTP.on("/list", HTTP_GET, handleFileList);     // list directory
+    HTTP.on("/edit", HTTP_GET, []() {
+        if (!handleFileRead("/edit.htm")) HTTP.send(404, "text/plain", "FileNotFound");
+    });                                                 // Загрузка редактора editor
+    HTTP.on("/edit", HTTP_PUT, handleFileCreate);       // Создание файла
+    HTTP.on("/edit", HTTP_DELETE, handleFileDelete);    // Удаление файла
+    // first callback is called after the request has ended with all parsed arguments
+    // second callback handles file uploads at that location
+    HTTP.on("/edit", HTTP_POST, []() {
+        HTTP.send(200, "text/plain", "");
+    }, handleFileUpload);
+    // called when the url is not defined here
+    // use it to load content from SPIFFS
+    HTTP.onNotFound([]() {
+        if (!handleFileRead(HTTP.uri()))
+        HTTP.send(404, "text/plain", "FileNotFound");
+    });                                                 // Файл не найден
+    // Принудительное сохранение config файла по запросу вида http://IP/save?save=ok
+    HTTP.on("/save", save_Config);
     // SSDP дескриптор
     HTTP.on("/description.xml", HTTP_GET, []() {SSDP.schema(HTTP.client()); });
     HTTP.begin();
